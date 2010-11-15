01cb4c883c1959cc31876874c461e5b7e48365ff
Christopher Olah <christopherolah.co@gmail.com>
2010-11-15 14:17:38 -0500

    decimal display in parameter tables

--- src/driver/config/MixedValueJTable.java
+++ src/driver/config/MixedValueJTable.java
@@ -7,16 +7,35 @@ import java.awt.Component;
 
 import javax.swing.DefaultCellEditor;
 import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.table.TableCellEditor;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
+import java.text.DecimalFormat;
+import javax.swing.table.DefaultTableCellRenderer;
+
 
 import cobweb.params.CobwebSelectionParam;
 
 class MixedValueJTable extends JTable {
 
 	private static final long serialVersionUID = -9106510371599896107L;
+
+	private final class PerciseDecimalTableCellRenderer extends DefaultTableCellRenderer {
+
+		private DecimalFormat formater = new DecimalFormat();
+
+		PerciseDecimalTableCellRenderer(){
+			super.setHorizontalAlignment(RIGHT);
+		}
+		
+		public Component getTableCellRenderComponent
+			(JTable tbl, Object value, boolean selected, boolean focused, int row, int col){
+			value = formater.format(value);
+			return super.getTableCellRendererComponent(tbl, value, selected, focused, row, col);
+		}
+	}
 
 	public static class CobwebSelectionEditor extends DefaultCellEditor {
 
@@ -77,10 +96,17 @@ class MixedValueJTable extends JTable {
 		TableColumn tableColumn = getColumnModel().getColumn(column);
 		TableCellRenderer renderer = tableColumn.getCellRenderer();
 		if (renderer == null && getValueAt(row, column) != null) {
-			renderer = getDefaultRenderer(getValueAt(row, column).getClass());
+			if (getValueAt(row, column) instanceof Double ||
+					getValueAt(row, column) instanceof Float ){
+				renderer = new PerciseDecimalTableCellRenderer();
+			}else {
+				renderer = getDefaultRenderer(getValueAt(row, column).getClass());
+			}
+			System.out.println(renderer.getClass().getName());
 		}
 		if (renderer == null) {
 			renderer = getDefaultRenderer(getColumnClass(column));
+			System.out.println(getColumnClass(column).getName());
 		}
 		return renderer;
 
