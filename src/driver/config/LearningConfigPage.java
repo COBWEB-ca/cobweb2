228591f2d3edfed57567bd2326831530a3ad75a3
Igor <igor.yeremin@gmail.com>
2011-05-02 15:31:26 -0400

    made learning configuration tab more consistant with others

--- src/driver/config/LearningConfigPage.java
+++ src/driver/config/LearningConfigPage.java
@@ -1,7 +1,6 @@
 package driver.config;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -15,12 +14,15 @@ public class LearningConfigPage implements ConfigPage {
 	private JTable learnTable;
 
 	public LearningConfigPage(LearningAgentParams[] params) {
-		ConfigTableModel ctm = new ConfigTableModel(params, "Learning");
+		ConfigTableModel ctm = new ConfigTableModel(params, "Agent");
 		learnTable = new MixedValueJTable();
 		learnTable.setModel(ctm);
 		JScrollPane sp = new JScrollPane(learnTable);
-		sp.setPreferredSize(new Dimension(400, 500));
+
 		GUI.makeGroupPanel(sp, "Agent Learning Parameters");
+		GUI.colorHeaders(learnTable, true);
+		learnTable.getColumnModel().getColumn(0).setPreferredWidth(150);
+
 		learningPanel = new JPanel(new BorderLayout());
 		learningPanel.add(sp, BorderLayout.NORTH);
 	}
