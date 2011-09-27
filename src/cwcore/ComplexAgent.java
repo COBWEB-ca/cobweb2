768482b21059031ea54d40e2c8e1ccdfc4976cd7
Igor <igor.yeremin@gmail.com>
2011-09-27 16:04:55 -0400

    fixed StepMutator mixing up to/from locations

--- src/cwcore/ComplexAgent.java
+++ src/cwcore/ComplexAgent.java
@@ -1105,7 +1105,7 @@ public class ComplexAgent extends cobweb.Agent implements cobweb.TickScheduler.C
 		}
 
 		for (StepMutator m : stepMutators)
-			m.onStep(this, getPosition(), destPos);
+			m.onStep(this, destPos, getPosition());
 
 		move(destPos);
 
