f5961dc44027711af72889bd1a09dee9fee08bf9
Igor <igor.yeremin@gmail.com>
2014-11-06 21:27:53 -0500

    Include a test that runs a full simulation to make sure it doesn't crash

---
+++ src/test/java/cobweb2test/SimulationTest.java
@@ -0,0 +1,18 @@
+package cobweb2test;
+
+import junit.framework.TestCase;
+import driver.CobwebApplicationRunner;
+
+/**
+ * Run common simulations to make sure they do not crash.
+ * Only runs one simulation right now because CobwebApplicationRunner.main cannot be called twice.
+ */
+public class SimulationTest extends TestCase {
+
+	/**
+	 * Runs everything.xml
+	 */
+	public void testExperimentEverythingXml() {
+		CobwebApplicationRunner.main("experiments/everything.xml", "", true, 1000, false);
+	}
+}
