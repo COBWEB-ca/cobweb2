4c03ccfc6965ddd89dcfd4c6c4e841369b0d2670
Igor <igor.yeremin@gmail.com>
2015-01-14 14:39:19 -0500

    Remove custom serialization from FoodwebParams

--- src/main/java/org/cobweb/cobweb2/core/params/FoodwebParams.java
+++ src/main/java/org/cobweb/cobweb2/core/params/FoodwebParams.java
@@ -4,20 +4,17 @@
 package org.cobweb.cobweb2.core.params;
 
 import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.cobweb.cobweb2.core.AgentFoodCountable;
 import org.cobweb.io.ConfDisplayName;
-import org.cobweb.io.ParameterCustomSerializable;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.cobweb.io.ConfList;
+import org.cobweb.io.ConfSquishParent;
+import org.cobweb.io.ParameterSerializable;
 
 /**
  * Food web parameters for an agent.
  */
-public class FoodwebParams implements ParameterCustomSerializable {
+public class FoodwebParams implements ParameterSerializable {
 
 	private static final long serialVersionUID = 1380425322335531943L;
 
@@ -25,77 +22,30 @@ public class FoodwebParams implements ParameterCustomSerializable {
 	 * If this agent can eat Agent type 1 canEatAgents[0] is true.
 	 */
 	@ConfDisplayName("Agent")
-	public boolean[] canEatAgent;
+	@ConfSquishParent
+	@ConfList(indexName = "agent", startAtOne = true)
+	public boolean[] canEatAgent = new boolean[0];
 
 	/**
 	 * If this agent can eat Food type 1 canEatFood[0] is true.
 	 */
 	@ConfDisplayName("Food")
-	public boolean[] canEatFood;
-
-	private final AgentFoodCountable env;
-
-	private static final Pattern AgentN = Pattern.compile("agent(\\d+)");
-	private static final Pattern FoodN = Pattern.compile("food(\\d+)");
+	@ConfSquishParent
+	@ConfList(indexName = "food", startAtOne = true)
+	public boolean[] canEatFood = new boolean[0];
 
 	public FoodwebParams(AgentFoodCountable env) {
-		this.env = env;
-
-		canEatFood = new boolean[env.getFoodTypes()];
-		canEatAgent = new boolean[env.getAgentTypes()];
-		for (int i = 0; i < canEatFood.length; i++) {
-			canEatFood[i] = true;
-		}
-	}
-
-	@Override
-	public void loadConfig(Node root) throws IllegalArgumentException {
-		canEatFood = new boolean[env.getFoodTypes()];
-		canEatAgent = new boolean[env.getAgentTypes()];
-
-		NodeList nodes = root.getChildNodes();
-
-		for (int i = 0; i < nodes.getLength(); i++) {
-			Node n = nodes.item(i);
-			String name = n.getNodeName();
-			Matcher m;
-			if ((m = AgentN.matcher(name)).matches()) {
-				int agent = Integer.parseInt(m.group(1)) - 1;
-				if (agent >= env.getAgentTypes())
-					continue;
-				canEatAgent[agent] = Boolean.parseBoolean(n.getFirstChild().getNodeValue());
-			} else if ((m = FoodN.matcher(name)).matches()) {
-				int food = Integer.parseInt(m.group(1)) - 1;
-				if (food >= env.getFoodTypes())
-					continue;
-				canEatFood[food] = Boolean.parseBoolean(n.getFirstChild().getNodeValue());
-			}
-		}
-	}
-
-	@Override
-	public void saveConfig(Node root, Document document) {
-		for (int agent = 0; agent < env.getAgentTypes(); agent++) {
-			Node n = document.createElement("agent" + (agent + 1));
-			n.setTextContent(Boolean.toString(canEatAgent[agent]));
-			root.appendChild(n);
-		}
-		for (int food = 0; food < env.getFoodTypes(); food++) {
-			Node n = document.createElement("food" + (food + 1));
-			n.setTextContent(Boolean.toString(canEatFood[food]));
-			root.appendChild(n);
-		}
+		resize(env);
 	}
 
 	public void resize(AgentFoodCountable envParams) {
-		{
-			boolean[] n = Arrays.copyOf(canEatAgent, envParams.getAgentTypes());
-			canEatAgent = n;
+		canEatAgent = Arrays.copyOf(canEatAgent, envParams.getAgentTypes());
 
-		}
-		{
-			boolean[] n = Arrays.copyOf(canEatFood, envParams.getFoodTypes());
-			canEatFood = n;
+		int oldSize = canEatFood.length;
+		canEatFood  = Arrays.copyOf(canEatFood, envParams.getFoodTypes());
+		// agents can eat all food by default
+		for (int i = oldSize; i < canEatFood.length; i++) {
+			canEatFood [i] = true;
 		}
 	}
 
