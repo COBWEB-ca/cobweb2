9ca03195450d2e259f7de2c5df18836f6bcd55f3
Igor <igor.yeremin@gmail.com>
2015-01-14 16:35:46 -0500

    Comments

--- src/main/java/org/cobweb/cobweb2/genetics/GeneticParams.java
+++ src/main/java/org/cobweb/cobweb2/genetics/GeneticParams.java
@@ -38,12 +38,21 @@ public class GeneticParams implements ParameterCustomSerializable {
 		RandomRecombination("Random Recombination"),
 		GeneSwapping("Gene Swapping");
 
+		/**
+		 * User-friendly name for mode
+		 */
 		private final String value;
 
 		private MeiosisMode(String s) {
 			value = s;
 		}
 
+		/**
+		 * Gets MeiosisMode from friendly name
+		 * @param s friendly name
+		 * @return MeiosisMode with given name
+		 * @throws IllegalArgumentException when no MeiosisMode has given friendly name
+		 */
 		public static MeiosisMode fromString(String s) {
 			for (MeiosisMode m : MeiosisMode.values()) {
 				if (m.value.equals(s))
