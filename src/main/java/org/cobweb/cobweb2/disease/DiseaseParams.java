9ac70b7ae224a03fe89b81cfa528e936ddd0dae8
Igor <igor.yeremin@gmail.com>
2015-01-14 13:28:37 -0500

    Remove custom serialization from DiseaseParams, uses save/loadArray now

--- src/main/java/org/cobweb/cobweb2/disease/DiseaseParams.java
+++ src/main/java/org/cobweb/cobweb2/disease/DiseaseParams.java
@@ -1,19 +1,15 @@
 package org.cobweb.cobweb2.disease;
 
 import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.cobweb.cobweb2.core.AgentFoodCountable;
 import org.cobweb.cobweb2.interconnect.Phenotype;
 import org.cobweb.io.ConfDisplayName;
+import org.cobweb.io.ConfList;
 import org.cobweb.io.ConfXMLTag;
-import org.cobweb.io.ParameterCustomSerializable;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.cobweb.io.ParameterSerializable;
 
-public class DiseaseParams implements ParameterCustomSerializable {
+public class DiseaseParams implements ParameterSerializable {
 
 	private static final long serialVersionUID = 6866958975246266955L;
 
@@ -26,12 +22,6 @@ public class DiseaseParams implements ParameterCustomSerializable {
 	@ConfXMLTag("initialInfection")
 	@ConfDisplayName("Initially infected fraction")
 	public float initialInfection = 0;
-
-	/**
-	 * Agent types this agent can transmit the disease to.
-	 */
-	@ConfDisplayName("Transmit to")
-	public boolean[] transmitTo;
 
 	/**
 	 * Chance this agent will get a disease from contact with an infected agent.
@@ -61,58 +51,6 @@ public class DiseaseParams implements ParameterCustomSerializable {
 	@ConfDisplayName("Factor")
 	public float factor = 2;
 
-	private final AgentFoodCountable env;
-
-	private static final Pattern agentTransmit = Pattern.compile("^agent(\\d+)$");
-
-	public DiseaseParams(AgentFoodCountable env) {
-		this.env = env;
-		transmitTo = new boolean[env.getAgentTypes()];
-	}
-
-	@Override
-	public void loadConfig(Node root) throws IllegalArgumentException {
-		transmitTo = new boolean[env.getAgentTypes()];
-
-		NodeList nl = root.getChildNodes();
-		for (int i = 0; i < nl.getLength(); i++) {
-			Node n = nl.item(i);
-			if (!n.getNodeName().equals("transmitTo"))
-				continue;
-			NodeList nl2 = n.getChildNodes();
-			for (int j = 0; j < nl2.getLength(); j++) {
-				Node tt = nl2.item(j);
-				String name = tt.getNodeName();
-
-				Matcher m = agentTransmit.matcher(name);
-				if (!m.matches())
-					continue;
-
-				int id = Integer.parseInt(m.group(1)) - 1;
-				if (id >= env.getAgentTypes())
-					continue;
-				boolean transmit = Boolean.parseBoolean(tt.getTextContent());
-				transmitTo[id] = transmit;
-			}
-		}
-	}
-
-	@Override
-	public void saveConfig(Node root, Document document) {
-		Node trans = document.createElement("transmitTo");
-		for (int i = 0; i < transmitTo.length; i++) {
-			Node n = document.createElement("agent" + (i + 1));
-			n.setTextContent(Boolean.toString(transmitTo[i]));
-			trans.appendChild(n);
-		}
-		root.appendChild(trans);
-
-	}
-
-	public void resize() {
-		boolean[] n = Arrays.copyOf(transmitTo, env.getAgentTypes());
-		this.transmitTo = n;
-	}
 
 	@ConfXMLTag("vaccinator")
 	@ConfDisplayName("Vaccinator")
@@ -133,5 +71,26 @@ public class DiseaseParams implements ParameterCustomSerializable {
 	@ConfXMLTag("recoveryTime")
 	@ConfDisplayName("Recovery time")
 	public int recoveryTime = 0;
+
+	/**
+	 * Agent types this agent can transmit the disease to.
+	 */
+	@ConfDisplayName("Transmit to")
+	@ConfXMLTag("transmitTo")
+	@ConfList(indexName = "agent", startAtOne = true)
+	public boolean[] transmitTo;
+
+
+	private final AgentFoodCountable env;
+
+	public DiseaseParams(AgentFoodCountable env) {
+		this.env = env;
+		transmitTo = new boolean[env.getAgentTypes()];
+	}
+
+	public void resize() {
+		boolean[] n = Arrays.copyOf(transmitTo, env.getAgentTypes());
+		this.transmitTo = n;
+	}
 
 }
