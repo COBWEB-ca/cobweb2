f9655d25cc580d04326c27439c69fc9782c5a682
Igor <igor.yeremin@gmail.com>
2015-01-29 16:51:03 -0500

    Mark serializable fields in DiseaseState

--- src/main/java/org/cobweb/cobweb2/plugins/disease/DiseaseState.java
+++ src/main/java/org/cobweb/cobweb2/plugins/disease/DiseaseState.java
@@ -1,11 +1,20 @@
 package org.cobweb.cobweb2.plugins.disease;
 
 import org.cobweb.cobweb2.plugins.AgentState;
+import org.cobweb.io.ConfXMLTag;
 
 public class DiseaseState implements AgentState {
+
+	@ConfXMLTag("sick")
 	public boolean sick = false;
+
+	@ConfXMLTag("vaccinated")
 	public boolean vaccinated = false;
+
+	@ConfXMLTag("sickStart")
 	public long sickStart = -1;
+
+	@ConfXMLTag("vaccineEffectiveness")
 	public float vaccineEffectiveness;
 
 	public DiseaseState(boolean sick, boolean vaccinated, long sickStart) {
