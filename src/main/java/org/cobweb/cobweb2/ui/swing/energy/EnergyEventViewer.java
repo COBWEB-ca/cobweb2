e1c32455b1fa23f4dd247bf22c43c26263c121db
Igor <igor.yeremin@gmail.com>
2015-02-02 00:59:54 -0500

    Move createFilterConfigPanel into separate class

--- src/main/java/org/cobweb/cobweb2/ui/swing/energy/EnergyEventViewer.java
+++ src/main/java/org/cobweb/cobweb2/ui/swing/energy/EnergyEventViewer.java
@@ -1,29 +1,18 @@
 package org.cobweb.cobweb2.ui.swing.energy;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-import javax.swing.AbstractAction;
 import javax.swing.BoxLayout;
-import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTree;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
-import javax.swing.tree.DefaultTreeCellRenderer;
-import javax.swing.tree.TreePath;
 
 import org.cobweb.cobweb2.Simulation;
-import org.cobweb.cobweb2.plugins.stats.CauseTree.CauseTreeNode;
 import org.cobweb.cobweb2.plugins.stats.EnergyStats;
 import org.cobweb.cobweb2.ui.swing.DisplayOverlay;
 import org.cobweb.cobweb2.ui.swing.DisplayPanel;
@@ -119,7 +108,7 @@ public class EnergyEventViewer extends OverlayPluginViewer<EnergyEventViewer> im
 		sp.setPreferredSize(new Dimension(10, 130));
 		mainPanel.add(sp);
 
-		JComponent filter = createFilterConfigPanel(energyStats);
+		JComponent filter = EnergyEventConfig.createFilterConfigPanel(energyStats);
 		mainPanel.add(filter);
 
 		result.pack();
@@ -139,94 +128,6 @@ public class EnergyEventViewer extends OverlayPluginViewer<EnergyEventViewer> im
 		JScrollPane sp = new JScrollPane(paramTable);
 		Util.makeGroupPanel(sp, "Display Options");
 		return sp;
-	}
-
-	private static JComponent createFilterConfigPanel(final EnergyStats energyStats) {
-
-		final CauseTreeModel treeModel = new CauseTreeModel(energyStats.causeTree);
-		final JTree tree = new JTree(treeModel);
-
-		tree.setCellRenderer(new DefaultTreeCellRenderer() {
-			@Override
-			public Component getTreeCellRendererComponent(JTree t, Object value, boolean sel, boolean expanded,
-					boolean leaf, int row, boolean focus) {
-
-				Component res = super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);
-				CauseTreeNode node = (CauseTreeNode)value;
-				setText(node.getName());
-
-				if (energyStats.whiteList.contains(node.type)) {
-					setForeground(Color.GREEN);
-				} else if (energyStats.blackList.contains(node.type)) {
-					setForeground(Color.RED);
-				} else if (!energyStats.isWatching(node.type)) {
-					setForeground(Color.LIGHT_GRAY);
-				}
-				return res;
-			}
-			private static final long serialVersionUID = 1L;
-		});
-
-		for (int i = 0; i < tree.getRowCount(); i++) {
-			tree.expandRow(i);
-		}
-
-		JScrollPane treePane = new JScrollPane(tree);
-
-		JButton whiteList = new JButton(new AbstractAction("Whitelist") {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				TreePath path = tree.getSelectionPath();
-				if (path == null)
-					return;
-
-				CauseTreeNode node = (CauseTreeNode) path.getLastPathComponent();
-				energyStats.whitelist(node.type);
-				treeModel.fireNodeChanged(path);
-			}
-			private static final long serialVersionUID = 1L;
-		});
-
-		JButton blackList = new JButton(new AbstractAction("Blacklist") {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				TreePath path = tree.getSelectionPath();
-				if (path == null)
-					return;
-
-				CauseTreeNode node = (CauseTreeNode) path.getLastPathComponent();
-				energyStats.blacklist(node.type);
-				treeModel.fireNodeChanged(path);
-			}
-			private static final long serialVersionUID = 1L;
-		});
-
-		JButton remove = new JButton(new AbstractAction("Unlist") {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				TreePath path = tree.getSelectionPath();
-				if (path == null)
-					return;
-
-				CauseTreeNode node = (CauseTreeNode) path.getLastPathComponent();
-				energyStats.unlist(node.type);
-				treeModel.fireNodeChanged(path);
-			}
-			private static final long serialVersionUID = 1L;
-		});
-
-		JPanel buttons = new JPanel();
-		buttons.add(whiteList);
-		buttons.add(blackList);
-		buttons.add(remove);
-
-
-		JPanel result = new JPanel(new BorderLayout());
-		result.add(treePane);
-		result.add(buttons, BorderLayout.SOUTH);
-
-		Util.makeGroupPanel(result, "Energy Cause Filter");
-		return result;
 	}
 
 }
