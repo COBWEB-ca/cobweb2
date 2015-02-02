05adf290980a7cdf4d48a3308612cc4dd029a2a9
Igor <igor.yeremin@gmail.com>
2015-02-01 19:49:23 -0500

    Remove extra class

--- src/main/java/org/cobweb/cobweb2/ui/swing/energy/EnergyEventViewer.java
+++ src/main/java/org/cobweb/cobweb2/ui/swing/energy/EnergyEventViewer.java
@@ -1,10 +1,14 @@
 package org.cobweb.cobweb2.ui.swing.energy;
 
+import org.cobweb.cobweb2.Simulation;
+import org.cobweb.cobweb2.plugins.stats.EnergyStats;
+import org.cobweb.cobweb2.ui.swing.DisplayOverlay;
 import org.cobweb.cobweb2.ui.swing.DisplayPanel;
+import org.cobweb.cobweb2.ui.swing.OverlayGenerator;
 import org.cobweb.cobweb2.ui.swing.OverlayPluginViewer;
 
 
-public class EnergyEventViewer extends OverlayPluginViewer<EnergyOverlay> {
+public class EnergyEventViewer extends OverlayPluginViewer<EnergyEventViewer> implements OverlayGenerator {
 
 
 	public EnergyEventViewer(DisplayPanel panel) {
@@ -17,7 +21,14 @@ public class EnergyEventViewer extends OverlayPluginViewer<EnergyOverlay> {
 	}
 
 	@Override
-	protected EnergyOverlay createOverlay() {
-		return new EnergyOverlay();
+	protected EnergyEventViewer createOverlay() {
+		return this;
+	}
+
+
+	@Override
+	public DisplayOverlay getDrawInfo(Simulation sim) {
+		EnergyDrawInfo res = new EnergyDrawInfo(sim.theEnvironment.getPlugin(EnergyStats.class));
+		return res;
 	}
 }
