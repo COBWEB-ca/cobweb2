b574aac6937884863b38f62fbd4b2fab5d62052c
Igor <igor.yeremin@gmail.com>
2015-02-02 04:02:14 -0500

    Save regional statistics to file

--- src/main/java/org/cobweb/cobweb2/ui/swing/CobwebApplication.java
+++ src/main/java/org/cobweb/cobweb2/ui/swing/CobwebApplication.java
@@ -539,7 +539,7 @@ public class CobwebApplication extends JFrame {
 		if (simulatorUI != null) {
 			viewers.add(new ProductionViewer(simulatorUI.displayPanel));
 			viewers.add(new EnergyEventViewer(simulatorUI.displayPanel, simRunner.getSimulation()));
-			viewers.add(new RegionViewer(simulatorUI.displayPanel));
+			viewers.add(new RegionViewer(simulatorUI.displayPanel, simRunner.getSimulation()));
 		}
 	}
 
--- src/main/java/org/cobweb/cobweb2/ui/swing/stats/RegionViewer.java
+++ src/main/java/org/cobweb/cobweb2/ui/swing/stats/RegionViewer.java
@@ -1,13 +1,20 @@
 package org.cobweb.cobweb2.ui.swing.stats;
 
+import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.FileDialog;
+import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-import javax.swing.BoxLayout;
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
@@ -15,6 +22,8 @@ import javax.swing.event.TableModelListener;
 import org.cobweb.cobweb2.Simulation;
 import org.cobweb.cobweb2.ui.GridStats;
 import org.cobweb.cobweb2.ui.GridStats.RegionOptions;
+import org.cobweb.cobweb2.ui.RegionStatsReporter;
+import org.cobweb.cobweb2.ui.UserInputException;
 import org.cobweb.cobweb2.ui.swing.DisplayOverlay;
 import org.cobweb.cobweb2.ui.swing.DisplayPanel;
 import org.cobweb.cobweb2.ui.swing.OverlayGenerator;
@@ -31,6 +40,8 @@ public class RegionViewer extends OverlayPluginViewer<RegionViewer> implements O
 
 	private JFrame configFrame;
 
+	private Simulation simulation;
+
 	public static class RegionViewerOptions implements ParameterSerializable {
 		@ConfDisplayName("Stats")
 		public RegionOptions statsOptions = new RegionOptions();
@@ -44,8 +55,9 @@ public class RegionViewer extends OverlayPluginViewer<RegionViewer> implements O
 		private static final long serialVersionUID = 1L;
 	}
 
-	public RegionViewer(DisplayPanel panel) {
+	public RegionViewer(DisplayPanel panel, Simulation simulation) {
 		super(panel);
+		this.simulation = simulation;
 	}
 
 	@Override
@@ -92,13 +104,35 @@ public class RegionViewer extends OverlayPluginViewer<RegionViewer> implements O
 
 
 	private JFrame createConfigFrame() {
-		JFrame result = new JFrame("Regional Statistics");
+		final JFrame result = new JFrame("Regional Statistics");
 		Container mainPanel = result.getContentPane();
-		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+		mainPanel.setLayout(new BorderLayout());
 
 		JComponent sp = createDisplayConfigPanel();
 		sp.setPreferredSize(new Dimension(300, 200));
 		mainPanel.add(sp);
+
+		JButton saveButton = new JButton(new AbstractAction("Save Data") {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				FileDialog theDialog = new FileDialog(result,
+						"Choose a file to save regional stats to", FileDialog.SAVE);
+				theDialog.setVisible(true);
+				if (theDialog.getFile() != null) {
+					try (PrintWriter writer = new PrintWriter(theDialog.getDirectory() + theDialog.getFile())) {
+
+						GridStats stats = new GridStats(simulation, viewerOptions.statsOptions);
+						RegionStatsReporter.report(writer, stats);
+					} catch (IOException ex) {
+						throw new UserInputException("Can't create report file!", ex);
+					}
+				}
+			}
+			private static final long serialVersionUID = 1L;
+		});
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.add(saveButton);
+		mainPanel.add(buttonPanel, BorderLayout.SOUTH);
 
 		result.pack();
 
