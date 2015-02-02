78b7f393dc335259fbee7a3fb2e7f9c5d55475b4
Igor <igor.yeremin@gmail.com>
2015-02-01 22:00:40 -0500

    Add up stats for all energy changes, grouped by cause

--- src/main/java/org/cobweb/cobweb2/plugins/stats/EnergyStats.java
+++ src/main/java/org/cobweb/cobweb2/plugins/stats/EnergyStats.java
@@ -1,7 +1,10 @@
 package org.cobweb.cobweb2.plugins.stats;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.cobweb.cobweb2.core.Agent;
 import org.cobweb.cobweb2.core.Cause;
@@ -9,6 +12,7 @@ import org.cobweb.cobweb2.core.LocationDirection;
 import org.cobweb.cobweb2.core.Topology;
 import org.cobweb.cobweb2.plugins.EnergyMutator;
 import org.cobweb.cobweb2.plugins.EnvironmentMutator;
+import org.cobweb.cobweb2.plugins.stats.CauseTree.CauseTreeNode;
 
 
 public class EnergyStats implements EnergyMutator, EnvironmentMutator {
@@ -18,8 +22,33 @@ public class EnergyStats implements EnergyMutator, EnvironmentMutator {
 	public float min;
 	private Topology topo;
 
+	public static class CauseStats {
+		public CauseStats(CauseTreeNode node) {
+			this.node = node;
+		}
+		public int count = 0;
+		public double totalDelta = 0;
+		public CauseTreeNode node;
+
+		@Override
+		public String toString() {
+			return node.getName() + " count: " + count + " total: " + totalDelta;
+		}
+	}
+
+	public Map<Class<? extends Cause>, CauseStats> causeStats = new HashMap<>();
+
+	public CauseTree causeTree = new CauseTree();
+
 	public EnergyStats(Topology topo) {
 		this.topo = topo;
+
+		Iterator<CauseTreeNode> iterator = causeTree.iterator();
+		while (iterator.hasNext()) {
+			CauseTreeNode node = iterator.next();
+			CauseStats stats = new CauseStats(node);
+			causeStats.put(node.type, stats);
+		}
 	}
 
 	public boolean collect = false;
@@ -30,10 +59,12 @@ public class EnergyStats implements EnergyMutator, EnvironmentMutator {
 		if (loc == null)
 			return;
 
+		Class<? extends Cause> causeClass = cause.getClass();
+
 		if (!whiteList.isEmpty()) {
 			boolean found = false;
 			for (Class<? extends Cause> c : whiteList)
-				if (c.isAssignableFrom(cause.getClass())) {
+				if (c.isAssignableFrom(causeClass)) {
 					found = true;
 					break;
 				}
@@ -42,9 +73,11 @@ public class EnergyStats implements EnergyMutator, EnvironmentMutator {
 		}
 
 		for (Class<? extends Cause> c : blackList) {
-			if (c.isAssignableFrom(cause.getClass()))
+			if (c.isAssignableFrom(causeClass))
 				return;
 		}
+
+		updateStats(delta, causeClass);
 
 		float newValue = map[loc.x][loc.y] + delta;
 		map[loc.x][loc.y] = newValue;
@@ -53,6 +86,15 @@ public class EnergyStats implements EnergyMutator, EnvironmentMutator {
 			max = newValue;
 		if (newValue < min)
 			min = newValue;
+	}
+
+	private void updateStats(int delta, Class<? extends Cause> causeClass) {
+		CauseStats stats = causeStats.get(causeClass);
+		do {
+			stats.count++;
+			stats.totalDelta += delta;
+			stats = causeStats.get(stats.node.parent.type);
+		} while (stats != null);
 	}
 
 	@Override
