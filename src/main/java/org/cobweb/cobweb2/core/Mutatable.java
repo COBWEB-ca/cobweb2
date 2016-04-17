dee612adb14ef487ff6dd2b35ed6be19abb94a27
Igor <igor.yeremin@gmail.com>
2016-04-16 20:43:24 -0400

    Tag all annotations @Documented to show up in javadoc

--- src/main/java/org/cobweb/cobweb2/core/Mutatable.java
+++ src/main/java/org/cobweb/cobweb2/core/Mutatable.java
@@ -1,5 +1,6 @@
 package org.cobweb.cobweb2.core;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -10,6 +11,7 @@ import java.lang.annotation.Target;
  */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface Mutatable {
 	// Nothing
 }
--- src/main/java/org/cobweb/io/ConfXMLTag.java
+++ src/main/java/org/cobweb/io/ConfXMLTag.java
@@ -1,5 +1,6 @@
 package org.cobweb.io;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -10,6 +11,7 @@ import java.lang.annotation.Target;
  */
 @Target({ElementType.FIELD, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface ConfXMLTag {
 	String value();
 }
