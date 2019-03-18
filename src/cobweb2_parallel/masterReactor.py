4692323a3cef938f981a1ef527c8eb101d932b3a
Zewen <41880356@qq.com>
2019-03-17 21:31:51 -0400

    further work

--- src/cobweb2_parallel/masterReactor.py
+++ src/cobweb2_parallel/masterReactor.py
@@ -30,3 +30,15 @@ class masterReactor:
             difference[i] = currentTotalEnergy[i] - previousTotalEnergy[i]
         return difference
 
+    def run(self):
+        pass
+
+
+def main(xml_configs):
+    pass
+    # numOfCobweb = len(xml_configs)
+    # for i in range(numOfCobweb):
+    #     cur_xml = xml_configs[i]
+    #     # -save {temp_xml} means the name of the xml after one tick of execution
+    #     temp_xml = "temp" + cur_xml
+    #     os.system(f"java -jar {jarfile} -autorun 1 -open {cur_xml} -hide -save {temp_xml}")
--- src/cobweb2_parallel/subReactor.py
+++ src/cobweb2_parallel/subReactor.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as et
 et.register_namespace('', 'http://cobweb.ca/schema/cobweb2/population')
+from shutil import copyfile
 
 class subReactor:
     def __init__(self, typeNumberA, typeNumberB, xmlA, xmlB, reactRule):
@@ -9,9 +10,46 @@ class subReactor:
         self.typeNumberA = typeNumberA
         self.typeNumberB = typeNumberB
         self.xmlA = xmlA
+        self.xmlATemp = 'temp_' + xmlA
+        copyfile(self.xmlA, self.xmlATemp)
         self.xmlB = xmlB
+        self.xmlBTemp = 'temp_' + xmlB
+        copyfile(self.xmlB, self.xmlBTemp)
         self.reactRule = reactRule
         # Key: (Type a, Type b) Value: react factor
+
+    def getAgentsEnergy(self, xmlPath):
+        """
+        Input: the path of the xml file
+        Output: a dictionary whose key is the agent type, value is the total energy of the corresponding agent type.
+        """
+        totalEnergy = {}
+        tree = et.parse(xmlPath)
+        root = tree.getroot()
+        for child in root:
+            agentType = int(child.attrib["type"])
+            energy = int(child[1].text) # gives the content of energy
+            totalEnergy[agentType] = totalEnergy.get(agentType, 0) + energy
+        return totalEnergy
+
+    def compare(self, previousTotalEnergy, currentTotalEnergy):
+        """
+        Return the energy change between previous state and current state
+        """
+        difference = {}
+        agentTypes = len(currentTotalEnergy)
+        assert agentTypes == len(previousTotalEnergy)
+        for i in range(1, agentTypes+1):
+            difference[i] = currentTotalEnergy[i] - previousTotalEnergy[i]
+        return difference
+
+    def foo(self):
+        """
+        Executed after we run the program using xmlA, and saved new data to xmlATemp
+        """
+        previousTotalEnergy = self.getAgentsEnergy(self.xmlA)
+        currentTotalEnergy = self.getAgentsEnergy(self.xmlATemp)
+        return self.react(self.compare(previousTotalEnergy, currentTotalEnergy))
 
     def react(self, comparisonDifference):
         reactResult = {}
@@ -31,6 +69,6 @@ class subReactor:
         tree.write(self.xmlB)
     
 if __name__ == "__main__":
-    a = subReactor(2, 2, "", "2d.xml", {})
+    a = subReactor(2, 2, "2d.1.xml", "2d.xml", {})
     reactResult = {2: 5, 4: -1}
     a.apply(reactResult)
